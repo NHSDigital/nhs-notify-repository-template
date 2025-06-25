38b1d040bdbb1dc10f5c1576d40d8e8b4f715cc4
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-06-25 10:02:52 +0100

    CCM-10591: Add Lambda Boilerplate

--- lambdas/example-lambda/src/index.ts
+++ lambdas/example-lambda/src/index.ts
@@ -1,3 +1,5 @@
+// Replace me with the actual code for your Lambda function
+// This is a simple example of an AWS Lambda function written in TypeScript.
 export const handler = async () => {
   console.log('Lambda function has been invoked.');
 };
