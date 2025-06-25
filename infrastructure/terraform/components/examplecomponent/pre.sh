b8b5110dd7a831f11d75b8ed824f7b3b98393775
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-06-25 10:14:58 +0100

    CCM-10591: Add Lambda Boilerplate

--- infrastructure/terraform/components/examplecomponent/pre.sh
+++ infrastructure/terraform/components/examplecomponent/pre.sh
@@ -1,3 +1,7 @@
+# This script is run before the Terraform apply command.
+# It ensures all Node.js dependencies are installed, generates any required dependencies,
+# and builds all Lambda functions in the workspace before Terraform provisions infrastructure.
+
 npm ci
 
 npm run generate-dependencies --workspaces --if-present
