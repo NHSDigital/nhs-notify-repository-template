732e9d193ead642d2ec57013b1c2b54ef7099b68
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-06-26 14:49:45 +0100

    CCM-10591: Lambda Boilerplate

--- infrastructure/terraform/components/examplecomponent/pre.sh
+++ infrastructure/terraform/components/examplecomponent/pre.sh
@@ -1,9 +1,9 @@
-# This script is run before the Terraform apply command.
-# It ensures all Node.js dependencies are installed, generates any required dependencies,
-# and builds all Lambda functions in the workspace before Terraform provisions infrastructure.
+# # This script is run before the Terraform apply command.
+# # It ensures all Node.js dependencies are installed, generates any required dependencies,
+# # and builds all Lambda functions in the workspace before Terraform provisions infrastructure.
 
-npm ci
+# npm ci
 
-npm run generate-dependencies --workspaces --if-present
+# npm run generate-dependencies --workspaces --if-present
 
-npm run lambda-build --workspaces --if-present
+# npm run lambda-build --workspaces --if-present
--- lambdas/example-lambda/src/index.ts
+++ lambdas/example-lambda/src/index.ts
@@ -1,1 +1,9 @@
-// Replace me with the actual code for your Lambda function
+import { Handler } from 'aws-lambda';
+
+export const handler: Handler = async (event) => {
+  console.log('Received event:', event);
+  return {
+    statusCode: 200,
+    body: 'Event logged',
+  };
+};
