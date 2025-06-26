11d301a6427dea1a719810ea3b207761b0b1b149
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-06-26 15:38:54 +0100

    CCM-10591: Lambda Boilerplate

--- lambdas/example-lambda/src/__tests__/index.test.ts
+++ lambdas/example-lambda/src/__tests__/index.test.ts
@@ -1,10 +1,11 @@
 import { handler } from '../index';
+import type { Context } from 'aws-lambda';
 import { mockDeep } from 'jest-mock-extended';
 
 describe('event-logging Lambda', () => {
   it('logs the input event and returns 200', async () => {
     const event = { foo: 'bar' };
-    const context = mockDeep<any>();
+    const context = mockDeep<Context>();
     const callback = jest.fn();
     const result = await handler(event, context, callback);
 
