6275eab0a5e9af6e9665901aaba6197315de77ba
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-06-26 14:55:45 +0100

    CCM-10591: Lambda Boilerplate

--- lambdas/example-lambda/src/__tests__/index.test.ts
+++ lambdas/example-lambda/src/__tests__/index.test.ts
@@ -1,1 +1,28 @@
-// Replace me with the actual tests for your Lambda function
+import { handler } from '../index';
+
+describe('event-logging Lambda', () => {
+  let logSpy: jest.SpyInstance;
+
+  beforeAll(() => {
+    logSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    logSpy.mockClear();
+  });
+
+  afterAll(() => {
+    logSpy.mockRestore();
+  });
+
+  it('logs the input event and returns 200', async () => {
+    const event = { foo: 'bar' };
+    const result = await handler(event, {} as any, () => undefined);
+
+    expect(logSpy).toHaveBeenCalledWith('Received event:', event);
+    expect(result).toEqual({
+      statusCode: 200,
+      body: 'Event logged',
+    });
+  });
+});
