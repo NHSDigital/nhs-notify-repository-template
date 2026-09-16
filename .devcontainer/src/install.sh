3d0d028979c2e89b80e5667312641e4232e15d89
aidenvaines-cgi <aiden.vaines@cgi.com>
2026-09-16 10:50:05 +0100

    CCM-9336: arch support

--- .devcontainer/src/install.sh
+++ .devcontainer/src/install.sh
@@ -4,8 +4,16 @@ cp ./postcreatecommand.sh /postcreatecommand.sh
 cp ./zshrc.template /.zshrc
 
 rm -Rf /.asdf
+
+ARCH=$(uname -m)
+case "$ARCH" in
+  x86_64) ASDF_ARCH="linux-amd64" ;;
+  aarch64|arm64) ASDF_ARCH="linux-arm64" ;;
+  *) echo "Unsupported architecture: $ARCH" && exit 1 ;;
+esac
+
 ASDF_VERSION=$(curl --proto "=https" -fsSL https://api.github.com/repos/asdf-vm/asdf/releases/latest | grep '"tag_name"' | cut -d '"' -f4)
-curl --proto "=https" -fL "https://github.com/asdf-vm/asdf/releases/download/${ASDF_VERSION}/asdf-${ASDF_VERSION}-linux-amd64.tar.gz" | tar -xz -C /usr/local/bin
+curl --proto "=https" -fL "https://github.com/asdf-vm/asdf/releases/download/${ASDF_VERSION}/asdf-${ASDF_VERSION}-${ASDF_ARCH}.tar.gz" | tar -xz -C /usr/local/bin
 asdf version
 
 cat /.zshrc
